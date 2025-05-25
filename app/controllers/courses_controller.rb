558c40fc40943936d89b14e9bedc04346a839ffc
saiindirat <tholusaiindira@gmail.com>
2025-05-25 07:23:47 +0000

    my commit

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,53 +1,50 @@
 class CoursesController < ApplicationController
   def index
     @courses = Course.all.order({ :created_at => :desc })
-
     render({ :template => "courses/index" })
   end
 
   def show
     the_id = params.fetch("path_id")
-    @course = Course.where({:id => the_id }).at(0)
-
+    @course = Course.where({ :id => the_id }).at(0)
     render({ :template => "courses/show" })
   end
 
   def create
     @course = Course.new
     @course.title = params.fetch("query_title")
-    @course.term_offered = params.fetch("query_term_")
+    @course.term_offered = params.fetch("query_term")
     @course.department_id = params.fetch("query_department_id")
 
     if @course.valid?
       @course.save
       redirect_to("/courses", { :notice => "Course created successfully." })
     else
-      redirect_to("/courses", { :notice => "Course failed to create successfully." })
+      redirect_to("/courses", { :alert => "Course failed to create successfully." })
     end
   end
 
   def update
-    id = params.fetch("path_id")
+    the_id = params.fetch("path_id")
     @course = Course.where({ :id => the_id }).at(0)
 
     @course.title = params.fetch("query_title")
-    @course.term_offered = params.fetch("query_term_offered")
+    @course.term_offered = params.fetch("query_term")
     @course.department_id = params.fetch("query_department_id")
 
     if @course.valid?
       @course.save
-      redirect_to("/courses/#{@course.id}", { :notice => "Course updated successfully."} )
+      redirect_to("/courses/#{@course.id}", { :notice => "Course updated successfully." })
     else
       redirect_to("/courses/#{@course.id}", { :alert => "Course failed to update successfully." })
     end
   end
 
   def destroy
-    the_id = params.fetch("path")
+    the_id = params.fetch("path_id")
     @course = Course.where({ :id => the_id }).at(0)
-
     @course.destroy
 
-    redirect_to("/courses", { :notice => "Course deleted successfully."} )
+    redirect_to("/courses", { :notice => "Course deleted successfully." })
   end
 end
--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -1,14 +1,12 @@
 class DepartmentsController < ApplicationController
   def index
     @departments = Department.all.order({ :created_at => :desc })
-
     render({ :template => "departments/index" })
   end
 
   def show
     the_id = params.fetch("path_id")
-    @department = Department.where({:id => the_id })
-
+    @department = Department.where({ :id => the_id }).at(0)
     render({ :template => "departments/show" })
   end
 
@@ -20,19 +18,18 @@ class DepartmentsController < ApplicationController
       @department.save
       redirect_to("/departments", { :notice => "Department created successfully." })
     else
-      redirect_to("/departments", { :notice => "Department failed to create successfully." })
+      redirect_to("/departments", { :alert => "Department failed to create successfully." })
     end
   end
 
   def update
     the_id = params.fetch("path_id")
     @department = Department.where({ :id => the_id }).at(0)
-
     @department.name = params.fetch("query_name")
 
     if @department.valid?
       @department.save
-      redirect_to("/departments/#{@department.id}", { :notice => "Department updated successfully."} )
+      redirect_to("/departments/#{@department.id}", { :notice => "Department updated successfully." })
     else
       redirect_to("/departments/#{@department.id}", { :alert => "Department failed to update successfully." })
     end
@@ -41,9 +38,8 @@ class DepartmentsController < ApplicationController
   def destroy
     the_id = params.fetch("path_id")
     @department = Department.where({ :id => the_id }).at(0)
-
     @department.destroy
 
-    redirect_to("/departments", { :notice => "Department deleted successfully."} )
+    redirect_to("/departments", { :notice => "Department deleted successfully." })
   end
 end
